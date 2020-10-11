2d34ec841619bd8c6ad444fe863f29093fd05a8a
h2uh2u <gmldnjs526@naver.com>
2020-10-12 01:04:56 +0900

    Fix admin.py, Add some records and superuser

--- exhibition/admin.py
+++ exhibition/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from exhibition.models import UserWithTitle
+from exhibition.models import Position
 from exhibition.models import Exhibit
 
+class UserWithTitleAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'title')
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('position_id', 'posx', 'posy', 'posz', 'roty')
+
+class ExhibitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'summary', 'position')
+
 # Register your models here.
-admin.site.register(Exhibit)
+admin.site.register(UserWithTitle, UserWithTitleAdmin)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Exhibit, ExhibitAdmin)
