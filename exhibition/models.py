aaf2ae12bc7bf5dd048ce32fa51dc519a5766aec
h2uh2u <gmldnjs526@naver.com>
2020-10-11 21:31:35 +0900

    Fix models (UserWithTitle, Position, Exhibit)

--- exhibition/models.py
+++ exhibition/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserWithTitle(AbstractUser):
+    title = models.CharField(max_length=150, blank=True)
+
+class Position(models.Model):
+    position_id = models.CharField(max_length=3, primary_key=True)
+    posx = models.FloatField()
+    posy = models.FloatField()
+    posz = models.FloatField()
+    roty = models.FloatField()
 
 class Exhibit(models.Model):
-    name = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=100)
     mesh = models.FileField(upload_to='mesh/')
-    info = models.TextField(default='')
-    posx = models.FloatField(default=0)
-    posy = models.FloatField(default=0)
-    posz = models.FloatField(default=0)
-    roty = models.FloatField(default=0)
+    summary = models.TextField(max_length=200)
+    info = models.TextField(max_length=2000)
+    position = models.ForeignKey(Position, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.name
