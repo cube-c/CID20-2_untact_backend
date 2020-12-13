8f1e12b239669ac3a9068def78a7d4dfb4042778
_cubec_ <cubecube1000@snu.ac.kr>
2020-12-13 17:43:53 +0900

    Comment resetting

--- message/apps.py
+++ message/apps.py
@@ -2,8 +2,10 @@ from django.apps import AppConfig
 
 class MessageConfig(AppConfig):
     name = 'message'
+    '''
     def ready(self):
         from .models import Invitation
         from exhibition.models import UserWithTitle as User
         Invitation.objects.all().delete()
         User.objects.all().update(channel_id="", consumer="", is_online=False)
+    '''
