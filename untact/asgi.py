639763407e977b91b5e8ef95dd4325e34d9abcdb
_cubec_ <cubecube1000@snu.ac.kr>
2020-12-13 18:52:05 +0900

    Fetch Django ASGI app early

--- untact/asgi.py
+++ untact/asgi.py
@@ -9,15 +9,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import message.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untact.settings')
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import message.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             message.routing.websocket_urlpatterns
